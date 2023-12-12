8dc165d9246a96ba71af2e32f72241f7d334292d
JaronG548 <jarongarza548@gmail.com>
2023-12-11 23:38:46 -0600

    Updated files

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
+//Variables for API to call upon again
 var APIKey = "72ff93aa90681f2746834fc0e03920bb"
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+//Elements pulled from the HTML to use
+let UserInput = document.querySelector("UserInput");
+let searchButton = document.querySelector("Search")
+
